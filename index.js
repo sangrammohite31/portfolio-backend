35576ae5e0d1c0e59e2ebe5dcbfcb50788e2cc1c
Sangram Mohite <142205738+sangrammohite31@users.noreply.github.com>
2024-07-21 21:39:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(cors({
-    origin: 'https://sangram-portfolio.vercel.app',
+    origin: '*',
   }));
 
 app.get('/api/get',async(req,res)=>{
